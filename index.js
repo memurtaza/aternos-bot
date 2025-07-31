14ded4361f891030da40577ff759bdbea2ce7d3e
memurtaza <alefiyadhedi66@gmail.com>
2025-07-31 12:05:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ catch {
 const mineflayer = require('mineflayer');
 
 // ✅ CONFIG
-const IP = '191.96.231.2';       // Minecraft server IP
-const PORT = 10578;              // Server port
+const IP = 'NameKiMkc.aternos.me';       // Minecraft server IP
+const PORT = 41475;              // Server port
 const WEB_PORT = process.env.PORT || 3000; // Fake port for Render/web host
 const PASSWORD = 'Mishra@123';
 
